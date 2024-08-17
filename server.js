3ed43c1dfdde24e7abe1fe88aa031340cfc07ab0
QuangVu <truongquangvu41@gmail.com>
2024-08-17 23:49:02 +0700

    Fixing typo

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ process.on('unhandledRejection', (err) => {
 });
 
 process.on('SIGTERM', () => {
-  console.log('💥SIGTERM RECEIVED, Shutting down gracefully');
+  console.log('💥SIGTERM RECEIVED. Shutting down gracefully');
   server.close(() => {
     console.log('💥Process terminated!');
   });
