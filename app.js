32b1a0c9a4b6d22485ba400d3705fd5e8a7721d8
QuangVu <truongquangvu41@gmail.com>
2024-08-18 11:41:50 +0700

    Deleted redundent options cors

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(cors());
 // Access-Control-Allow-Origin=*
 
-app.options('*', cors());
+// app.options('*', cors());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
