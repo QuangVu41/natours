20d8685de7ae6a47a7e0b654008d57e527bda9d0
QuangVu <truongquangvu41@gmail.com>
2024-08-18 16:42:06 +0700

    Testing send cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,7 @@ const signToken = (id) =>
   });
 
 const createSendToken = (user, statusCode, req, res) => {
+  console.log(req.headers);
   const token = signToken(user._id);
   const cookieOptions = {
     expires: new Date(
