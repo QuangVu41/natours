272356f992e9d271203418a0dffda89aca07719c
QuangVu <truongquangvu41@gmail.com>
2024-08-18 19:24:10 +0700

    Testing secure req 2

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,13 +13,14 @@ const signToken = (id) =>
 
 const createSendToken = (user, statusCode, req, res) => {
   console.log(req.secure);
+  console.log(req.headers['x-forwarded-proto']);
   const token = signToken(user._id);
   const cookieOptions = {
     expires: new Date(
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
     ),
     httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    secure: req.secure,
   };
 
   res.cookie('jwt', token, cookieOptions);
