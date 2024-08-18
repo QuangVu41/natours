49e0236efbd5f0252c1691310c4b2c922f3e3501
QuangVu <truongquangvu41@gmail.com>
2024-08-18 17:15:01 +0700

    Disabled xForwardedForHeader

--- app.js
+++ app.js
@@ -52,6 +52,7 @@ const limiter = rateLimit({
   limit: 100,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, please try again in an hour!',
+  validate: { xForwardedForHeader: false },
 });
 app.use('/api', limiter);
 
